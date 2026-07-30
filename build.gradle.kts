b48f6302c662e854a46470387578ea4358236d22
conty1975conty-collab <conty1975conty@gmail.com>
2026-07-30 05:04:45 -0500

    ci(android): declare kotlin compose plugin version in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
-﻿plugins {
+plugins {
     // Указаны версии плагинов; Android Studio / Gradle может предложить обновления.
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    // Добавляем явную версию для плагина Compose, иначе Gradle не найдёт его при указанном плагине в модуле app
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
 }
